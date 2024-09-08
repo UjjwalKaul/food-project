b38f5dd53538db4fb757cc20f447e07a605648d7
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-08 20:03:29 +0530

    Linked dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,13 @@
 import LogoutButton from '@/components/LogoutButton';
+import Link from 'next/link';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-slate-900">
       <header className="sticky top-0 z-10 flex justify-between p-5 shadow-md items-center">
-        <div className="text-3xl font-bold">Dashboard</div>
+        <Link className="text-3xl font-bold" href="/">
+          Dashboard
+        </Link>
         <LogoutButton />
       </header>
       <main>{children}</main>
