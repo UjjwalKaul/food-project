a9d5dd8384afcc191a193114a3407c5adbe27957
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-09 17:05:43 +0530

    added blank target

--- components/SearchItem.tsx
+++ components/SearchItem.tsx
@@ -39,6 +39,7 @@ const SearchItem: React.FC<{ recipe: Recipe }> = ({ recipe }) => {
         </p>
         <a
           href={recipe.url}
+          target="_blank"
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Read more
           <svg
