d3af8ab04a79146c50ed70da776abb5efc62f054
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-09 14:10:44 +0530

    Added correct middleware

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import LogoutButton from '@/components/LogoutButton';
 import Link from 'next/link';
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <header className="sticky top-0 z-10 flex justify-between p-5 shadow-md items-center bg-slate-800 mb-12">
