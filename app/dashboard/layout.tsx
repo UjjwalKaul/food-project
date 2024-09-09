393cde53f10fdd1dc98008252e441812a61344b0
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-09 14:46:38 +0530

    Align logo

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,25 @@
 import LogoutButton from '@/components/LogoutButton';
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '../assets/logo.png';
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <header className="sticky top-0 z-10 flex justify-between p-5 shadow-md items-center bg-slate-800 mb-12">
-        <Link className="text-3xl font-bold" href="/dashboard">
-          Dashboard
+        <Link
+          className="flex items-center gap-2 text-3xl font-bold"
+          href="/dashboard">
+          <Image
+            className="mb-[0.6rem]"
+            src={logo}
+            width={35}
+            height={35}
+            alt="Logo"
+          />
+          <span>Dashboard</span>
         </Link>
+
         <LogoutButton />
       </header>
       <main>{children}</main>
